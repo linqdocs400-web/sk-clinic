d93cc116d8427204671ee8fae88fa24865e47014
K Sumanth <ksumanth3085@gmail.com>
2026-07-06 17:58:20 +0530

    Simplify Google Map iframe implementation

--- src/components/Visit.tsx
+++ src/components/Visit.tsx
@@ -70,14 +70,7 @@ export function Visit() {
 
           {/* Google Map */}
           <div className="overflow-hidden rounded-lg bg-cream shadow-[0_30px_60px_-30px_rgba(0,0,0,0.25)] h-[420px] lg:h-auto min-h-[420px]">
-            <iframe
-              title="Siri and Kaushik's Skin Hair STD Dermatosurgery and Laser Clinic"
-              className="h-full w-full border-0"
-              loading="lazy"
-              allowFullScreen
-              referrerPolicy="strict-origin-when-cross-origin"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3807.989557360587!2d78.522059!3d17.3642355!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb99088bacf7ed%3A0x7ce50f1[...]"
-            />
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3807.989557360587!2d78.522059!3d17.3642355!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb99088bacf7ed%3A0x7ce50f18e5e29b8c!2sSiri%20and%20Kaushik&#39;s%20Skin%20Hair%20STD%20Dermatosurgery%20and%20Laser%20clinic!5e0!3m2!1sen!2sin!4v1783340821593!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="strict-origin-when-cross-origin"></iframe>
           </div>
         </div>
       </div>
